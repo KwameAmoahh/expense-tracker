a01bcff850bfcc99594164c6554e5de4ac2a0c46
KwameAmoahh <kwame.amoah498@gmail.com>
2026-03-23 23:35:57 +0200

    Added remaining amount card

--- expense-tracker-angular/src/app/app.ts
+++ expense-tracker-angular/src/app/app.ts
@@ -2,12 +2,14 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Navbar } from './components/common/navbar/navbar';
 import { TotalSpentCard } from './components/dashboard/total-spent-card/total-spent-card';
+import { RemainingAmountCard } from './components/dashboard/remaining-amount-card/remaining-amount-card';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet,
             Navbar,
-            TotalSpentCard
+            TotalSpentCard,
+            RemainingAmountCard
   ],
   templateUrl: './app.html',
   styleUrl: './app.css'
