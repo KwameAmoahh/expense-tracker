fcd5e1794d448952d74b14b6d1f108535f5b4aa0
KwameAmoahh <kwame.amoah498@gmail.com>
2026-03-23 23:42:55 +0200

    Added to dashboard

--- expense-tracker-angular/src/app/app.ts
+++ expense-tracker-angular/src/app/app.ts
@@ -3,13 +3,15 @@ import { RouterOutlet } from '@angular/router';
 import { Navbar } from './components/common/navbar/navbar';
 import { TotalSpentCard } from './components/dashboard/total-spent-card/total-spent-card';
 import { RemainingAmountCard } from './components/dashboard/remaining-amount-card/remaining-amount-card';
+import { TransactionCountCard } from './components/dashboard/transaction-count-card/transaction-count-card';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet,
             Navbar,
             TotalSpentCard,
-            RemainingAmountCard
+            RemainingAmountCard,
+            TransactionCountCard
   ],
   templateUrl: './app.html',
   styleUrl: './app.css'
